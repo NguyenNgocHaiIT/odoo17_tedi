b54d207c9b1cd6cec95e574f5b2c7ccc30d52631
Nguyễn Xuân Thưởng <121345028+thuongerikdev@users.noreply.github.com>
2025-11-24 11:23:07 +0700

    add import attendance

--- addons_tedi/hr_attendance_tedi/__manifest__.py
+++ addons_tedi/hr_attendance_tedi/__manifest__.py
@@ -5,18 +5,21 @@
     'author': 'Thưởng',
     'sequence': 1,
     'summary': '',
-    'depends': ['base','web', 'hr' ,"hr_attendance" , 'hr_tedi'],
+    'depends': ['base', 'web', 'hr', 'hr_attendance', 'hr_tedi'],
     'data': [
         "security/ir.model.access.csv",
-
+        "wizard/attendance_import_views.xml",
         "views/attendance_views.xml",
         "views/holiday_config_views.xml",
-
         "views/menu.xml",
-
     ],
-
-
+    'assets': {
+        'web.assets_backend': [
+            'hr_attendance_tedi/static/src/js/list_import_button.js',
+            # TẠM THỜI BỎ XML NÀY RA, NÓ LÀM VỠ ASSETS
+            # 'hr_attendance_tedi/static/src/xml/list_import_button.xml',
+        ],
+    },
     'installable': True,
     'application': True,
     'auto_install': False,
--- addons_tedi/hr_attendance_tedi/models/__init__.py
+++ addons_tedi/hr_attendance_tedi/models/__init__.py
@@ -1,2 +1,3 @@
 from . import attendance
 from . import  holiday_config
+from . import tedi_attendance_import_wizard
